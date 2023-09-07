6415292d6f8f10e5c79ceb24838236139ea0b0b4
kkrasta <basitmohammed362@gmail.com>
2023-09-07 02:57:03 +0000

    postheader

---
+++ ig-clone/Data/posts.js
@@ -0,0 +1,183 @@
+import { USERS} from "./users";
+
+export const POSTS =[
+    {
+        imageUrl: 'https://www.japantimes.co.jp/uploads/imported_images/uploads/2022/12/np_file_201058.jpeg',
+        user: USERS[0].user,
+        Likes: 8_000_000_000,
+        caption: 'Best moment of my life!!!',
+        profile_picture: USERS[0].image,
+
+        comments: [
+        {
+            user: 'C.ronaldo',
+            comment: 'Im so jealous of you!'
+
+        },
+
+        {
+            user: 'emi_martin',
+            comment: 'GOAT!'
+
+        },
+        {
+            user: 's_appiah',
+            comment: 'Aponkye mu aponkye ankasa'
+
+        },
+
+        ]
+    },
+
+    {
+        imageUrl: 'https://www.usnews.com/object/image/00000188-0c8b-d636-af8c-9eef53770000/gettyimages-163642085.jpg?update-time=1683837705683&size=responsive640',
+        user: USERS[1].user,
+        Likes: 7580,
+        caption: 'I hope this is the beginning of forever',
+        profile_picture: USERS[1].image,
+
+        comments: [
+        {
+            user: 'mobasit8',
+            comment: 'love go sweet o'
+
+        },
+
+        {
+            user: 'dimsonLee',
+            comment: 'Chaleeeee'
+
+        },
+        {
+            user: 'padddy007',
+            comment: 'So proud of you guys'
+
+        },
+
+        ]
+    },
+
+    {
+        imageUrl: 'https://i.scdn.co/image/ab67616d0000b2735fa86e334ded7b731b9ed8cd',
+        user: USERS[2].user,
+        Likes: 9_000,
+        caption: 'My new album ...To Memba is out now..lets stream',
+        profile_picture: USERS[2].image,
+
+        comments: [
+        {
+            user: 'mobasit_8',
+            comment: '🔥🔥🔥🔥🔥'
+
+        },
+
+        {
+            user: 'wixkidayo',
+            comment: 'Jiggy Fc!!❗❗❤️❤️'
+
+        },
+        {
+            user: 'burnagram',
+            comment: 'Ma guy 🔥🔥🔥🔥'
+
+        },
+
+        ]
+    },
+
+    {
+        imageUrl: 'https://www.si.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTk5MjY5Mjk2NDkxMjEwMTQy/ea-sports-fc-24-cover-image.jpg',
+        user: USERS[3].user,
+        Likes: 8_000,
+        caption: '@mobasit_8...be ready..ago lash you pass',
+        profile_picture: USERS[3].image,
+
+        comments: [
+        {
+            user: 'boyd_007',
+            comment: 'moff moff'
+
+        },
+
+        ]
+    },
+
+    {
+        imageUrl: 'https://media.gq-magazine.co.uk/photos/635b9d020ddd768a6c91021e/16:9/w_1919,h_1079,c_limit/Call%20of%20Duty%202810%20header.jpg',
+        user: USERS[4].user,
+        Likes: 750,
+        caption: 'modernwarfare!!!!!!!!!!!!!',
+        profile_picture: USERS[4].image,
+
+        comments: [
+        {
+            user: 'kwakuTee',
+            comment: 'chale ma mind dey'
+
+        },
+
+        {
+            user: 'simba',
+            comment: 'your money come oo'
+
+        },
+        
+        ]
+    },
+
+    {
+        imageUrl: 'https://static.vecteezy.com/system/resources/previews/008/252/907/original/fc-barcelona-logo-football-club-barcelona-crest-emblem-on-a-blaugrana-background-free-vector.jpg',
+        user: USERS[5].user,
+        Likes: 1_000_00,
+        caption: 'Hello Culers..Im here!',
+        profile_picture: USERS[5].image,
+
+        comments: [
+        {
+            user: 'jaO_felix',
+            comment: 'VAmossssss'
+
+        },
+
+        {
+            user: 'araujo07',
+            comment: 'we are back'
+
+        },
+        {
+            user: 'pedri__',
+            comment: 'come onnnnn'
+
+        },
+
+        ]
+    },
+
+    {
+        imageUrl: 'https://www.chilipeppermadness.com/wp-content/uploads/2022/08/Spicy-Noodles-SQ.jpg',
+        user: USERS[6].user,
+        Llikes: 58,
+        caption: 'cravings',
+        profile_picture: USERS[6].image,
+
+        comments: [
+        {
+            user: 'killers_0',
+            comment: 'this system you dey crave noodles?'
+
+        },
+
+        {
+            user: 'doc_george',
+            comment: 'eii..its 10pm'
+
+        },
+        {
+            user: 'kalyjay',
+            comment: 'chale me sef oo'
+
+        },
+
+        ]
+    },
+]
